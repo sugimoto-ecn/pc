019b97e083492a827e2d14aa67d6805e610c16d6
sugimoto-ecn <kein@encinc.com>
2021-11-18 16:36:35 +0900

    pwm

---
+++ pc_list/no8/work3.py
@@ -0,0 +1,31 @@
+from gpiozero import Button
+from gpiozero import LED
+from gpiozero import PWMLED
+ 
+p_button = Button(27, bounce_time=0.05)
+n_button = Button(22, bounce_time=0.05)
+
+led_pwm = PWMLED(17)
+led_pwm.value = 0.1
+ 
+def increment_pwm():
+    global pwm_value
+
+    pwm_value += 0.1
+    print('increment')
+
+def decrement_pwm():
+    global pwm_value
+
+    pwm_value -= 0.1
+    print("decrement")
+ 
+def main_loop():
+    global pwm_value
+    while True:
+        p_button.when_pressed = increment_pwm
+        n_button.when_pressed = decrement_pwm
+        led_pwm = PWMLED(led_pin)
+ 
+if __name__ == '__main__':
+    main_loop()
